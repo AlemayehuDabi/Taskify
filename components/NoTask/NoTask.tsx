d5a22451d89e4cf459b30491e3ec353b2cc50788
Alemayehu <alemayehudabi606@gmail.com>
2025-07-10 15:43:16 +0300

    feat: no-task page

--- components/NoTask/NoTask.tsx
+++ components/NoTask/NoTask.tsx
@@ -1,11 +1,24 @@
 'use client';
+import { Plus } from 'lucide-react';
 
 export default function NoTask() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div>
-        <div></div>
-        <div></div>
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center gap-8">
+        {/*text  */}
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="text-xl font-bold">No Task Yet</h2>
+
+          <span className=" flex flex-col justify-center items-center text-sm text-gray-600">
+            <p>No tasks created yet, You can start by clicking</p>
+            <p>the add new button bellow to create one</p>
+          </span>
+        </div>
+        {/* button */}
+        <button className="bg-primary py-2 px-6 text-xs text-gray-200 rounded-lg flex items-center gap-2">
+          <Plus size={16} />
+          Add New
+        </button>
       </div>
     </div>
   );
